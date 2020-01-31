c2870820aebd617a9ca2bae58d95a6c437291904
OlgaPavlyuk <ou.pavlyuk@gmail.com>
2020-01-31 16:35:10 +0300

    Updates

---
+++ precache-manifest.7a0ba6cd4b9b972f25e406542af9b1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d332b6523ac3caa40abddb4c337a1022",
+    "url": "/vk-auth/index.html"
+  },
+  {
+    "revision": "850d0340bd341f733e3b",
+    "url": "/vk-auth/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5d08b8a0de940a328e6",
+    "url": "/vk-auth/static/js/2.a5d505a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vk-auth/static/js/2.a5d505a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850d0340bd341f733e3b",
+    "url": "/vk-auth/static/js/main.af90fb48.chunk.js"
+  },
+  {
+    "revision": "e82471766e9e24bbcd5d",
+    "url": "/vk-auth/static/js/runtime-main.41d1ce6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vk-auth/static/media/logo.5d5d9eef.svg"
+  }
+]);
